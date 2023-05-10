d8cb9b2b5df01733a2b2eca1122c1dc68e572f5f
Ai Hotokebuchi <112920373+ai357@users.noreply.github.com>
2023-05-10 12:46:31 +0000

    favorites/controllers

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,12 +3,13 @@ class FavoritesController < ApplicationController
   # お気に入り登録
   def create
     post_image = PostImage.find(params[:post_image_id])
+    # ログイン中ユーザーのfavoritesカラムに新規作成
     favorite = current_user.favorites.new(post_image_id: post_image.id)
     favorite.save
     redirect_to post_image_path(post_image)
   end
 
-  # お気に入り
+  # お気に入り削除
   def destroy
     post_image = PostImage.find(params[:post_image_id])
     favorite = current_user.favorites.find_by(post_image_id: post_image.id)
