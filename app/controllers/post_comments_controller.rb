417bf807538fbc5baf95600953c58d40e203d204
Ai Hotokebuchi <112920373+ai357@users.noreply.github.com>
2023-05-11 13:51:17 +0000

    post_comments/controller

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,13 +1,17 @@
 class PostCommentsController < ApplicationController
 
+  # コメント機能
   def create
     post_image = PostImage.find(params[:post_image_id])
+    # ログイン中ユーザーのPost_commentsカラムの中に枠をつくる
     comment = current_user.post_comments.new(post_comment_params)
+    # 
     comment.post_image_id = post_image.id
     comment.save
     redirect_to post_image_path(post_image)
   end
 
+  # コメント削除
   def destroy
     PostComment.find(params[:id]).destroy
     redirect_to post_image_path(params[:post_image_id])
