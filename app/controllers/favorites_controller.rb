fdc2991fb4bbfde10f656ec673782af29911f566
Ai Hotokebuchi <112920373+ai357@users.noreply.github.com>
2023-05-11 21:44:06 +0000

    favorites/controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -15,6 +15,7 @@ class FavoritesController < ApplicationController
     post_image = PostImage.find(params[:post_image_id])
     # ログイン中ユーザーのfavoritesに入ってる情報を１IDからつ見つける
     favorite = current_user.favorites.find_by(post_image_id: post_image.id)
+    # お気に入り削除
     favorite.destroy
     redirect_to post_image_path(post_image)
   end
