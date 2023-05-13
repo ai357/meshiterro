74febfb67b784f7de426858701d64f2aa8b3b20c
Ai Hotokebuchi <112920373+ai357@users.noreply.github.com>
2023-05-13 07:59:54 +0000

    post_comments/contorller

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -14,6 +14,7 @@ class PostCommentsController < ApplicationController
 
   # コメント削除
   def destroy
+    # PostCommentモデルから見つけて削除する
     PostComment.find(params[:id]).destroy
     # 削除後、post_imageページに戻る
     redirect_to post_image_path(params[:post_image_id])
