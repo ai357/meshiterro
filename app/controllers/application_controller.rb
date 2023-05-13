4901f7b19c4027056c29804475b7af2dc523af71
Ai Hotokebuchi <112920373+ai357@users.noreply.github.com>
2023-05-13 07:57:40 +0000

    application/controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,12 @@ class ApplicationController < ActionController::Base
  before_action :authenticate_user!, except: [:top]
  before_action :configure_permitted_parameters, if: :devise_controller?
 
- # サインイン後の遷移先を
+ # サインイン後の遷移先を設定
  def after_sign_in_path_for(resource)
   post_image_index_path
  end
 
- #ログアウト後の遷移先  
+ #ログアウト後の遷移先を
  def after_sign_out_path_for(resource)
   about_path
  end
